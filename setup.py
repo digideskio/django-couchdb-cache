9ed82b724b04dbe48376b77b51d566cd9d04e539
Javier Angulo Lucerón <javier@shuttlecloud.com>
2015-03-11 12:56:54 +0100

    Added missing configuration to install examples

--- setup.py
+++ setup.py
@@ -1,18 +1,39 @@
-from setuptools import setup, find_packages
+from glob import glob
+import os
+from setuptools import setup
 
 PACKAGE_VERSION = '0.1'
+PACKAGE_NAME = 'django-couchdb-cache'
+
+EXAMPLES_TARGET_DIR = 'share/{}/examples'.format(PACKAGE_NAME)
+EXAMPLES_LOCAL_DIR = 'examples'
+EXAMPLES = (
+    'test_project',
+)
+
+
+def get_data_files():
+    data_files = []
+
+    for example in EXAMPLES:
+        target_dir = os.path.join(EXAMPLES_TARGET_DIR, example)
+        target_files = glob(os.path.join(EXAMPLES_LOCAL_DIR, example, '*'))
+        data_files.append((target_dir, target_files))
+
+    return data_files
 
 setup(
-    name='django-couchdb-cache',
+    name=PACKAGE_NAME,
     author='ShuttleCloud Corp',
     author_email='dev@shuttlecloud.com',
     description='CouchDB cache application for Django',
     url='https://github.com/shuttlecloud/django-couchdb-cache.git',
     version=PACKAGE_VERSION,
-    packages=find_packages(),
-    include_package_data=False,
+    packages=['couchdb_cache'],
+    include_package_data=True,
     zip_safe=False,
     scripts=[],
+    data_files=get_data_files(),
     install_requires=[
         'Django<=1.7.4',
         'CouchDB==0.10',
